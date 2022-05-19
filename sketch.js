17fe0b77d1e65ede36aa4cbbacb0373f0950d494
Anmol Gairola <anmolgairola123@gmail.com>
2022-05-19 23:14:54 +0530

    upload images

--- sketch.js
+++ sketch.js
@@ -1,32 +1,52 @@
-let img
-val = 5
-
-
-function preload(){
-  
-  img = loadImage('assets/img8.png')
-  
-}
-
+let input;
+let img;
+val = 10
+let value = 1
 
 function setup() {
-  //val = random(1, 10)
-  print(val)
-  createCanvas(img.width, img.height);
-  
-  for (let col = 0; col < img.width; col+=random(val)){
-    for(let row = 0; row < img.height; row+=random(val)){
+  input = createFileInput(handleFile);
+  input.position(0, 0);
+  can = createCanvas(500, 500)
+}
+
+function draw() {
+  //background(255);
+  if (img && value == 1) {
+    can = createCanvas(img.width, img.height)
+    image(img, 0, 0, width, height);
+    for (let col = 0; col < img.width; col+=val){
+      for(let row = 0; row < img.height; row+=val){
       
       x = col
       y = row
-            
-      
-      let c = img.get(x, y);
+        
+      let c = can.get(x, y);
       stroke(color(c))
       strokeWeight(random(val,20))
       circle(x, y, random(val));
       
-    }
+      
+      
+   }
+ }
+    value = value * -1
+      
   }
 }
 
+function handleFile(file) {
+  print(file);
+  if (file.type === 'image') {
+    img = createImg(file.data, '');
+    img.hide();
+    print(can)
+  } else {
+    img = null;
+  }
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    value = value * -1;
+  } 
+}
